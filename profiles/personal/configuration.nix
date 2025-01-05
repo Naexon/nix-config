a367c119b9f52d0eab8eed8e6c52a3f7751b53c1
Naexon <naexoncode@pm.me>
2025-01-05 18:42:33 +0100

    updated pulseaudio setting

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -74,14 +74,16 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+  services = {
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
